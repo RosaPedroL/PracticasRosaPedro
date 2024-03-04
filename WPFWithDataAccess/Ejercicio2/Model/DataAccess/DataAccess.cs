4df73e2a6c34a0b3d9cb3726eb63f48fe9810e96
RosaMPedro <161348890+RosaPedroL@users.noreply.github.com>
2024-03-04 12:33:13 -0600

    Subiendo el proyecto
    Se subió en el repositorio.

---
+++ WPFWithDataAccess/Ejercicio2/Model/DataAccess/DataAccess.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.DataAccess
+{
+    public class DataAccess
+    {
+        string databaseConnection = "Data Source=DESKTOP-K394INF;Initial Catalog=EscuelaDB;user id=sa;password=Notiene12345;";
+
+        public void SaveAlumno(Alumno alumno)
+        {
+            try
+            {
+                using(SqlConnection connection = new SqlConnection(databaseConnection))
+                {
+                    SqlCommand command = new SqlCommand("[Escuela].[SaveAlumno]", connection);
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    SqlParameter nombreParameter = new SqlParameter();
+                    nombreParameter.ParameterName = "Nombre";
+                    nombreParameter.Value = alumno.Nombre;
+
+                    SqlParameter apellidosParameter = new SqlParameter();
+                    apellidosParameter.ParameterName = "Apellidos";
+                    apellidosParameter.Value = alumno.Apellidos;
+
+                    SqlParameter promedioParameter = new SqlParameter();
+                    promedioParameter.ParameterName = "Promedio";
+                    promedioParameter.Value = alumno.Promedio;
+
+                    command.Parameters.Add(nombreParameter);
+                    command.Parameters.Add(apellidosParameter);
+                    command.Parameters.Add(promedioParameter);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+
+            }catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        public List<Alumno> GetAlumnosList()
+        {
+            List<Alumno> resultado = new List<Alumno>();
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(databaseConnection))
+                {
+                    SqlCommand command = new SqlCommand("[Escuela].[GetAlumnoList]", connection);
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    
+                    connection.Open();
+                    var reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Alumno alumno = new Alumno();
+                        alumno.Id = int.Parse(reader["Id"].ToString());
+                        alumno.Nombre = reader["Nombre"].ToString();
+                        alumno.Apellidos = reader["Apellidos"].ToString();
+                        alumno.Promedio = decimal.Parse(reader["Promedio"].ToString());
+                        resultado.Add(alumno);
+                    }
+
+                    connection.Close();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            return resultado;
+        }
+    }
+}
---
+++ WPFWithDataAccess/Ejercicio2/ViewModel/MainViewModel.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.Practices.Prism.Commands;
+using Microsoft.Practices.Prism.ViewModel;
+using Model;
+using Model.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ViewModel
+{
+    public class MainViewModel : NotificationObject
+    {
+        private DataAccess _dataAcces;
+        private string _nombre;
+        private string _apellidos;
+        private decimal _promedio;
+        private ICommand _agregarCommand;
+        private List<Alumno> _alumnos;
+        private Alumno _alumnoSeleccionado;
+
+        public string Nombre
+        {
+            get { return _nombre; }
+            set
+            {
+                if(value == _nombre) return;
+                _nombre = value;
+                RaisePropertyChanged(() => Nombre);
+            }
+        }
+        public string Apellidos
+        {
+            get { return _apellidos; }
+            set
+            {
+                if (value == _apellidos) return;
+                _apellidos = value;
+                RaisePropertyChanged(() => Apellidos);
+            }
+        }
+        public decimal Promedio
+        {
+            get { return _promedio; }
+            set
+            {
+                if (value == _promedio) return;
+                _promedio = value;
+                RaisePropertyChanged(() => Promedio);
+            }
+        }
+        public List<Alumno> Alumnos
+        {
+            get { return _alumnos; }
+            set
+            {
+                if (value == _alumnos) return;
+                _alumnos = value;
+                RaisePropertyChanged(() => Alumnos);
+            }
+        }
+        public Alumno AlumnoSeleccionado
+        {
+            get { return _alumnoSeleccionado; }
+            set
+            {
+                if (value == _alumnoSeleccionado) return;
+                _alumnoSeleccionado = value;
+
+                Nombre = _alumnoSeleccionado.Nombre;
+                Apellidos = _alumnoSeleccionado.Apellidos;
+                Promedio = _alumnoSeleccionado.Promedio;
+
+                RaisePropertyChanged(() => AlumnoSeleccionado);
+            }
+        }
+        public ICommand AgregarCommand
+        {
+            get
+            {
+                if(_agregarCommand == null)
+                {
+                    _agregarCommand = new DelegateCommand(AgregarAlumno, () => true);
+                }
+                return _agregarCommand;
+            }
+        }
+
+        public MainViewModel()
+        {
+            Alumnos = new List<Alumno>();
+            _dataAcces = new DataAccess();
+            Alumnos = _dataAcces.GetAlumnosList();
+        }
+
+
+
+        public void AgregarAlumno()
+        {
+            Alumno alumno = new Alumno();
+            alumno.Nombre = Nombre;
+            alumno.Apellidos = Apellidos;
+            alumno.Promedio = Promedio;
+            _dataAcces.SaveAlumno(alumno);
+            Alumnos = _dataAcces.GetAlumnosList();
+            Nombre = String.Empty;
+            Apellidos = String.Empty;
+            Promedio = 0;
+        }
+    }
+}
