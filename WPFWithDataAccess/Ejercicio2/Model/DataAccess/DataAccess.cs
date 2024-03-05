0e5f66be8b3cdffd46a9d777ffca7cc8f47d47d9
RosaMPedro <161348890+RosaPedroL@users.noreply.github.com>
2024-03-05 17:18:04 -0600

    Modelo y ViewModel
    Se han introducido nuevos métodos para las consultas en la base de datos dentro del Modelo. Además, en el ViewModel se han agregado llamadas a estos métodos del Modelo para modificar datos o ingresar desde la vista.

--- WPFWithDataAccess/Ejercicio2/Model/DataAccess/DataAccess.cs
+++ WPFWithDataAccess/Ejercicio2/Model/DataAccess/DataAccess.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -9,23 +10,25 @@ namespace Model.DataAccess
 {
     public class DataAccess
     {
-        string databaseConnection = "Data Source=DESKTOP-K394INF;Initial Catalog=EscuelaDB;user id=sa;password=Notiene12345;";
+        //string databaseConnection = "Data Source=DESKTOP-04QFPV5;Initial Catalog=EscuelaDB;user id=sa;password=Jb941005;";
+        string databaseConnection = ConfigurationManager.ConnectionStrings["MyConnectionBD"].ConnectionString;
 
         public void SaveAlumno(Alumno alumno)
         {
             try
             {
+                //buscar si existe el usuario notificar en un modak si no poner exitoso  
                 using(SqlConnection connection = new SqlConnection(databaseConnection))
                 {
                     SqlCommand command = new SqlCommand("[Escuela].[SaveAlumno]", connection);
                     command.CommandType = System.Data.CommandType.StoredProcedure;
                     SqlParameter nombreParameter = new SqlParameter();
                     nombreParameter.ParameterName = "Nombre";
-                    nombreParameter.Value = alumno.Nombre;
+                    nombreParameter.Value = alumno.Nombre.Trim(); // Este método devuelve una nueva cadena que no contiene espacios en blanco al principio ni al final de la cadena original
 
                     SqlParameter apellidosParameter = new SqlParameter();
                     apellidosParameter.ParameterName = "Apellidos";
-                    apellidosParameter.Value = alumno.Apellidos;
+                    apellidosParameter.Value = alumno.Apellidos.Trim();
 
                     SqlParameter promedioParameter = new SqlParameter();
                     promedioParameter.ParameterName = "Promedio";
@@ -53,14 +56,17 @@ namespace Model.DataAccess
             {
                 using (SqlConnection connection = new SqlConnection(databaseConnection))
                 {
+                    // Ejecutar una consulta
                     SqlCommand command = new SqlCommand("[Escuela].[GetAlumnoList]", connection);
                     command.CommandType = System.Data.CommandType.StoredProcedure;
-                    
+                    // Abrir la conexión
                     connection.Open();
+                    // Ejecutar la consulta y leer los resultados
                     var reader = command.ExecuteReader();
 
                     while (reader.Read())
                     {
+                        //// Acceder a los datos
                         Alumno alumno = new Alumno();
                         alumno.Id = int.Parse(reader["Id"].ToString());
                         alumno.Nombre = reader["Nombre"].ToString();
@@ -68,8 +74,89 @@ namespace Model.DataAccess
                         alumno.Promedio = decimal.Parse(reader["Promedio"].ToString());
                         resultado.Add(alumno);
                     }
-
+                    //cierra la conexion 
                     connection.Close();
+                }
+
+            }
+            catch (Exception ex)
+            { //capta el error. 
+                Console.WriteLine(ex.ToString());
+            }
+
+            return resultado;
+        }
+
+        public Alumno SearchAlumno(int id)
+        {
+            Alumno alumno = new Alumno();
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(databaseConnection)) 
+                {
+                    // Ejecutar una consulta
+                    SqlCommand command = new SqlCommand("[Escuela].[SearchAlumno]", connection);
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    // Parámetro para el procedimiento almacenado
+                    command.Parameters.AddWithValue("@Id", id);
+
+                    /* El método Parameters.AddWithValue se utiliza para agregar parámetros a un comando SQL en ADO.NET.
+                    Este método permite especificar los valores de los parámetros de manera más conveniente y segura. */
+                    
+                    // Abrir la conexión
+                    connection.Open();
+                    //Comando para leer los datos regreso de la consulta 
+                    var reader = command.ExecuteReader();
+                  
+                    while (reader.Read())
+                    {
+                        // Recuperar el valor devuelto por el procedimiento almacenado
+                        alumno.Id = int.Parse(reader["Id"].ToString());
+                        alumno.Nombre = reader["Nombre"].ToString();
+                        alumno.Apellidos = reader["Apellidos"].ToString();
+                        alumno.Promedio = decimal.Parse(reader["Promedio"].ToString());
+                    }
+                    reader.Close();
+
+                }
+
+            }catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            return alumno;
+        }
+
+        public int SearchFullNameAlumno(int Id,string Nombre, string Apellidos) //busca si ya existe un alumno con ese nombre y apellido 
+        {
+            int id=0;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(databaseConnection))
+                {
+                    // Ejecutar una consulta
+                    SqlCommand command = new SqlCommand("[Escuela].[SearchFullNameAlumno]", connection);
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    // Parámetro para el procedimiento almacenado
+                    command.Parameters.AddWithValue("@Id", Id);
+                    command.Parameters.AddWithValue("@Nombre", Nombre);
+                    command.Parameters.AddWithValue("@Apellidos", Apellidos);
+
+                  
+
+                    // Abrir la conexión
+                    connection.Open();
+                    //Comando para leer los datos regreso de la consulta 
+                    var reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        // Recuperar el valor devuelto por el procedimiento almacenado
+                        id = int.Parse(reader["Id"].ToString()); //si encuentra devuelve su id & si no devuelve cero 
+                    }
+                    reader.Close();
+
                 }
 
             }
@@ -78,7 +165,130 @@ namespace Model.DataAccess
                 Console.WriteLine(ex.ToString());
             }
 
-            return resultado;
+           return id;
         }
+
+        public int SearchFllAlumno(string Nombre, string Apellidos) //busca si ya existe un alumno con ese nombre y apellido 
+        {
+            int id = 0;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(databaseConnection))
+                {
+                    // Ejecutar una consulta
+                    SqlCommand command = new SqlCommand("[Escuela].[SearchFllAlumno]", connection);
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    // Parámetro para el procedimiento almacenado
+                    command.Parameters.AddWithValue("@Nombre", Nombre);
+                    command.Parameters.AddWithValue("@Apellidos", Apellidos);
+                    // Abrir la conexión
+                    connection.Open();
+                    //Comando para leer los datos regreso de la consulta 
+                    var reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        // Recuperar el valor devuelto por el procedimiento almacenado
+                        id = int.Parse(reader["Id"].ToString()); //si encuentra devuelve su id & si no devuelve cero 
+                    }
+                    reader.Close();
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            return id;
+        }
+
+        public void UpdateAlumno(Alumno alumno, out int existe, out int repetido) //out parameters para devolver múltiples valores desde un método.
+        {
+            existe = 0;
+            repetido = 0;
+            Alumno alumnobuscado = new Alumno();
+            try
+            {
+                alumnobuscado = SearchAlumno(alumno.Id);
+                if (!string.IsNullOrEmpty(alumnobuscado.Nombre)) //El método IsNullOrEmpty() toma una cadena como argumento y devuelve true si la cadena es null o tiene una longitud de cero; de lo contrario, devuelve false
+                {
+                    existe = 1;//si existe 
+                    int idrepetido = SearchFullNameAlumno(alumnobuscado.Id,alumnobuscado.Nombre.Trim(),alumnobuscado.Apellidos.Trim());
+                    //if (nombrecompleto.Equals(alumno.Nombre + alumno.Apellidos, StringComparison.OrdinalIgnoreCase) == false) //El método Equals() en C# se utiliza para comparar dos objetos y determinar si son iguales o no
+                    if(idrepetido==0) //si no esta repetido devuelve cero 
+                    {
+                        repetido = 0; //no esta repetido 
+                        using (SqlConnection connection = new SqlConnection(databaseConnection))
+                        {
+                            SqlCommand command = new SqlCommand("[Escuela].[UpdateAlumno]", connection);
+                            command.CommandType = System.Data.CommandType.StoredProcedure;
+                            // Parámetros para el procedimiento almacenado
+                            command.Parameters.AddWithValue("@Id", alumno.Id);
+                            command.Parameters.AddWithValue("@Nombre", alumno.Nombre);
+                            command.Parameters.AddWithValue("@Apellidos", alumno.Apellidos);
+                            command.Parameters.AddWithValue("@Promedio", alumno.Promedio);
+                            connection.Open();
+                            int filasAfectadas = command.ExecuteNonQuery();
+                            //Console.WriteLine($"Filas afectadas: {filasAfectadas}");
+                            connection.Close();
+                        }
+                    }
+                    else { repetido = 1; }
+                }
+              else { existe = 0; }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+           // return (existe, repetido);
+            
+        }
+
+        public int DeleteAlumno(int Id) {
+           int  existe = 0;
+            try
+            {
+                Alumno alumnobuscado = new Alumno();
+                alumnobuscado= SearchAlumno(Id);
+                if (!string.IsNullOrEmpty(alumnobuscado.Nombre)) //El método IsNullOrEmpty() toma una cadena como argumento y devuelve true si la cadena es null o tiene una longitud de cero; de lo contrario, devuelve false
+                {
+                    existe = 1;//si existe 
+                    using (SqlConnection connection = new SqlConnection(databaseConnection))
+                    {
+                        SqlCommand command = new SqlCommand("[Escuela].[DeleteAlumno]", connection);
+                        command.CommandType = System.Data.CommandType.StoredProcedure;
+                        // Parámetros para el procedimiento almacenado
+                        command.Parameters.AddWithValue("@Id", Id);
+                        connection.Open();
+                        int filasAfectadas = command.ExecuteNonQuery();
+                        //Console.WriteLine($"Filas afectadas: {filasAfectadas}");
+                        connection.Close();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return existe;
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
--- WPFWithDataAccess/Ejercicio2/ViewModel/MainViewModel.cs
+++ WPFWithDataAccess/Ejercicio2/ViewModel/MainViewModel.cs
@@ -5,8 +5,10 @@ using Model.DataAccess;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using System.Windows.Input;
 
 namespace ViewModel
@@ -14,19 +16,34 @@ namespace ViewModel
     public class MainViewModel : NotificationObject
     {
         private DataAccess _dataAcces;
+        private int _id;
         private string _nombre;
         private string _apellidos;
         private decimal _promedio;
-        private ICommand _agregarCommand;
-        private List<Alumno> _alumnos;
-        private Alumno _alumnoSeleccionado;
+        private ICommand _agregarCommand; //comando para agregar 
+        private ICommand _actualizarCommand;
+        private ICommand _eliminarCommand;
+        private ICommand _limpiarCommand; //comando para limpiar los campos
+        private List<Alumno> _alumnos; //lista del alumno 
+        private Alumno _alumnoSeleccionado; //alumno que selecciono 
+
+        public int Id
+        {
+            get { return _id; }
+            set
+            {
+                if (value == _id) return;
+                _id = value;
+                RaisePropertyChanged(() => Id);
+            }
+        }
 
         public string Nombre
         {
             get { return _nombre; }
             set
             {
-                if(value == _nombre) return;
+                if (value == _nombre) return;
                 _nombre = value;
                 RaisePropertyChanged(() => Nombre);
             }
@@ -51,7 +68,7 @@ namespace ViewModel
                 RaisePropertyChanged(() => Promedio);
             }
         }
-        public List<Alumno> Alumnos
+        public List<Alumno> Alumnos //envia los datos 
         {
             get { return _alumnos; }
             set
@@ -60,54 +77,218 @@ namespace ViewModel
                 _alumnos = value;
                 RaisePropertyChanged(() => Alumnos);
             }
+
         }
-        public Alumno AlumnoSeleccionado
+        public Alumno AlumnoSeleccionado //poner valor a los campos de la tabla seleccionado -> se utiliza en la vista esta propiedad   SelectedItem para seleecionar 
         {
             get { return _alumnoSeleccionado; }
             set
             {
                 if (value == _alumnoSeleccionado) return;
                 _alumnoSeleccionado = value;
-
-                Nombre = _alumnoSeleccionado.Nombre;
-                Apellidos = _alumnoSeleccionado.Apellidos;
-                Promedio = _alumnoSeleccionado.Promedio;
-
+                //no es necesario ponerlo en la vista aqui ya puedo utilizar el id sin especifica en la vista 
+                if (_alumnoSeleccionado != null)
+                {
+                    Id = _alumnoSeleccionado.Id;
+                    Nombre = _alumnoSeleccionado.Nombre;
+                    Apellidos = _alumnoSeleccionado.Apellidos;
+                    Promedio = _alumnoSeleccionado.Promedio;
+                }
+                else
+                {
+                    LimpiarCampos();
+                }
                 RaisePropertyChanged(() => AlumnoSeleccionado);
             }
         }
+
         public ICommand AgregarCommand
         {
             get
             {
-                if(_agregarCommand == null)
+                if (_agregarCommand == null)
                 {
                     _agregarCommand = new DelegateCommand(AgregarAlumno, () => true);
                 }
                 return _agregarCommand;
             }
         }
+        public ICommand LimpiarCommand
+        {
+            get
+            {
+                if (_limpiarCommand == null)
+                {
+                    _limpiarCommand = new DelegateCommand(LimpiarCampos, () => true);
+                }
+                return _limpiarCommand;
+            }
+        }
+
+        public ICommand ActualizarCommand
+        {
+            get
+            {
+                if (_actualizarCommand == null)
+                {
+                    _actualizarCommand = new DelegateCommand(ActualizarAlumno, () => true);
+                }
+                return _actualizarCommand;
+            }
+        }
+        public ICommand EliminarCommand
+        {
+            get
+            {
+                if (_eliminarCommand == null)
+                {
+                    _eliminarCommand = new DelegateCommand(EliminarAlumno, () => true);
+                }
+                return _eliminarCommand;
+            }
+        }
 
         public MainViewModel()
         {
             Alumnos = new List<Alumno>();
-            _dataAcces = new DataAccess();
-            Alumnos = _dataAcces.GetAlumnosList();
+            _dataAcces = new DataAccess(); //crear un objeto donde accede a la base de datos 
+            Alumnos = _dataAcces.GetAlumnosList(); //obtiene un inicio los datos 
+
         }
 
 
 
         public void AgregarAlumno()
         {
-            Alumno alumno = new Alumno();
+            DialogResult result;
+            Alumno alumno = new Alumno(); //se crea un nuevo dato (1)
             alumno.Nombre = Nombre;
+            //readyranche lista copiar 
+            //ahora si el alumno compia temporar alumno agrega todo los elementos alumno 
             alumno.Apellidos = Apellidos;
             alumno.Promedio = Promedio;
-            _dataAcces.SaveAlumno(alumno);
-            Alumnos = _dataAcces.GetAlumnosList();
+            result = MessageBox.Show("¿Estás seguro de agregar este dato?", "Confirmar agregar", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                if (string.IsNullOrEmpty(Nombre) || string.IsNullOrEmpty(Apellidos) || Promedio == 0)
+                {
+                    result = MessageBox.Show("Uno de los campos estan vació, por favor verifique que este lleno los campos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                }
+                else
+                {
+                    if (_dataAcces.SearchFllAlumno(Nombre, Apellidos) == 0)
+                    {
+                        _dataAcces.SaveAlumno(alumno);//se guarda en la bd (2)
+                        result = MessageBox.Show("El dato se agrego correctamente", "Inf", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        Alumnos = _dataAcces.GetAlumnosList();//para actualizar la tabla con el nuevo datos (3)
+                        LimpiarCampos();
+                  
+
+                    }
+                    else
+                    {
+                        result = MessageBox.Show("Esta repetido el dato", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+            }
+        }
+
+
+        public void LimpiarCampos()
+        {
+            Id = 0;
             Nombre = String.Empty;
             Apellidos = String.Empty;
             Promedio = 0;
+
+            // String.Empty ->Es equivalente a ""(una cadena de longitud cero).Se utiliza
+            // comúnmente en lugar de escribir una cadena vacía directamente en el código.
         }
+
+
+        public void ActualizarAlumno()
+        {
+            DialogResult result = MessageBox.Show("¿Estás seguro de que deseas actualizar este dato?", "Confirmar actualizar", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                if (string.IsNullOrEmpty(Nombre) || string.IsNullOrEmpty(Apellidos) || Promedio == 0)
+                {
+                    result = MessageBox.Show("Uno de los campos estan vació, por favor verifique que este lleno los campos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                }
+                else
+                {
+                    //variables para los valores de salida
+                    int existe;
+                    int repetido;
+                    Alumno alumno = new Alumno(); //se crea un nuevo dato (1)
+                    alumno.Id = Id;
+                    alumno.Nombre = Nombre;
+                    alumno.Apellidos = Apellidos;
+                    alumno.Promedio = Promedio;
+                    //  método UpdateAlumno y pasar las variables de salida
+                    _dataAcces.UpdateAlumno(alumno, out existe, out repetido);
+                    if (existe == 1 & repetido == 0)
+                    {
+                        result = MessageBox.Show("El dato se modifico", "Inf", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        Alumnos = _dataAcces.GetAlumnosList(); //para actualizar la tabla con el nuevo datos (3)   
+                                                               //AlumnoSeleccionado = null;
+
+                    }
+                    else
+                    {
+                        if (existe == 0)
+                        {
+                            result = MessageBox.Show("El dato no existe", "Detalle", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+
+                        }
+                        if (repetido == 1)
+                        {
+                            result = MessageBox.Show("Esta repetido el dato", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                        }
+                    }
+                }
+
+            }
+
+        }
+
+        public void EliminarAlumno()
+        {
+            DialogResult result = MessageBox.Show("¿Estás seguro de que deseas eliminar este dato?", "Confirmar eliminar", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                int existe = _dataAcces.DeleteAlumno(Id);
+                if (existe == 1)
+                {
+                    result = MessageBox.Show("El dato se elimino correctament", "Inf", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    Alumnos = _dataAcces.GetAlumnosList();
+
+                }
+                else
+                {
+                    result = MessageBox.Show("El dato no existe", "Detalle", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+
+                }
+
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
